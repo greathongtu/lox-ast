7dcbd350469be90798daa30b18e3b8c3739d865b
Aaron Zhang <aaron@Aarons-MacBook-Pro.local>
2023-04-19 20:26:31 +0800

    first day end

--- src/error.rs
+++ src/error.rs
@@ -4,10 +4,6 @@ pub struct LoxError {
     message: String,
 }
 impl LoxError {
-    pub fn error(line: usize, message: String) -> LoxError {
-        LoxError { line, message } 
-    }
-
     pub fn report(&self, loc: String) {
         eprintln!("[line {}] Error {}: {}", self.line, loc, self.message);
     }
