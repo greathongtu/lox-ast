73e356268fbe40c0cf113d101143f3bcc8684a3f
Aaron Zhang <aaron@Aarons-MBP.lan>
2023-04-20 23:15:29 +0800

    start working on metaprogramming the expr - chapter 5

--- generate-ast/target/debug/deps/generate_ast-730a5fa249ff6393.d
+++
@@ -1,5 +0,0 @@
-/Users/aaron/dev/rs/lox-ast/generate-ast/target/debug/deps/generate_ast-730a5fa249ff6393: src/main.rs
-
-/Users/aaron/dev/rs/lox-ast/generate-ast/target/debug/deps/generate_ast-730a5fa249ff6393.d: src/main.rs
-
-src/main.rs:
--- generate-ast/target/debug/generate-ast.d
+++
@@ -1,1 +0,0 @@
-/Users/aaron/dev/rs/lox-ast/generate-ast/target/debug/generate-ast: /Users/aaron/dev/rs/lox-ast/generate-ast/src/main.rs
